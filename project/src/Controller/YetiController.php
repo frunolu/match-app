287d749e929ed858567e9131d386afba7f037b2f
frunolu <fruno.lu@gmail.com>
2024-09-08 09:46:31 +0200

    make CRUD entity Yeti.php via maker bundle

--- project/src/Controller/YetiController.php
+++ project/src/Controller/YetiController.php
@@ -2,17 +2,82 @@
 
 namespace App\Controller;
 
+use App\Entity\Yeti;
+use App\Form\YetiType;
+use App\Repository\YetiRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class YetiController extends AbstractController
+#[Route('/yeti')]
+final class YetiController extends AbstractController
 {
-    #[Route('/yeti', name: 'app_yeti')]
-    public function index(): Response
+
+
+    #[Route(name: 'app_yeti_index', methods: ['GET'])]
+    public function index(YetiRepository $yetiRepository): Response
     {
         return $this->render('yeti/index.html.twig', [
-            'controller_name' => 'YetiController',
+            'yetis' => $yetiRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_yeti_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $yeti = new Yeti();
+        $form = $this->createForm(YetiType::class, $yeti);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($yeti);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_yeti_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('yeti/new.html.twig', [
+            'yeti' => $yeti,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_yeti_show', methods: ['GET'])]
+    public function show(Yeti $yeti): Response
+    {
+        return $this->render('yeti/show.html.twig', [
+            'yeti' => $yeti,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_yeti_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Yeti $yeti, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(YetiType::class, $yeti);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_yeti_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('yeti/edit.html.twig', [
+            'yeti' => $yeti,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_yeti_delete', methods: ['POST'])]
+    public function delete(Request $request, Yeti $yeti, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$yeti->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($yeti);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_yeti_index', [], Response::HTTP_SEE_OTHER);
     }
 }
